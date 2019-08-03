b8e812b5c8d7cd014113f8a64c3f09f1b26af557
Chaz <chaz@plaidpotion.com>
2019-08-03 17:03:21 -0400

    unbork file upload via API

--- src/service.py
+++ src/service.py
@@ -26,27 +26,27 @@ cgi.maxlen = 10 * 1024 * 1024 # 5MB file size limit for uploads
 
 global utilities
 utilities = Util()
-        
+
 class Index:
         """Loads the index page from the static dir"""
         def GET(self):
-                web.header('Content-Type','text/html; charset=utf-8')   
+                web.header('Content-Type','text/html; charset=utf-8')
                 raise web.seeother('/static/index.html')
-                
+
 class Extractor:
-        
+
         def GET(self, datafile, method):
-                
+
                 params = web.input(output="xml")
                 """Returns some extracted information from a file"""
                 extractor = Extraction()
                 data = ''
                 txtfile = TMP_FOLDER + datafile + '.txt'
-                
+
                 """Check if the file exists, if not return a 404"""
                 if not os.path.exists(txtfile):
                         return web.notfound()
-                
+
                 try:
                         if method == 'text':
                                 txtfile = TMP_FOLDER + datafile + '.txt'
@@ -72,20 +72,20 @@ class Extractor:
                                         return utilities.printXML(data)
                                 elif params.output == 'json':
                                         jsondata = xmltodict.parse(data)
-                                        web.header('Content-Type','text/json; charset=utf-8')   
+                                        web.header('Content-Type','text/json; charset=utf-8')
                                         return json.dumps(jsondata)
                                 else:
                                         web.ctx.status = '400'
                                         return 'Unsupported output format. Options are: "xml" (default) and "json"'
-                
+
                 except (IOError, OSError) as er: #Internal error, i.e. during extraction
                         web.debug(er)
                         return web.internalerror()
 
 class Handler(object):  # Super-class for the two handlers
-        
+
         def fileCheck(self, pdfpath):
-                        
+
                 try:
                         # After we handle the file upload, we do the following:
                         # I. Check the uploaded file's type -> proceed to next step
@@ -104,6 +104,7 @@ class Handler(object):  # Super-class for the two handlers
                         web.debug(typeFilterStatus)
                         web.debug("pdfpath is:")
                         web.debug(pdfpath)
+
                         if typeFilterStatus == "application/pdf":
                                 txtpath = utilities.pdf2text(pdfpath)
                         elif typeFilterStatus == "application/postscript":
@@ -135,19 +136,19 @@ class Handler(object):  # Super-class for the two handlers
                         elif acaFilterStatus == "0":
                                 return False, "Your document failed our academic document filter."
                 return True, typeFilterStatus
-                
+
         def printLocations(self, fileid):
                 location = web.ctx.homedomain + '/extractor/pdf/' + fileid
                 web.ctx.status = '201 CREATED'
                 web.header('Location', location)
-                web.header('Content-Type','text/xml; charset=utf-8') 
+                web.header('Content-Type','text/xml; charset=utf-8')
                 web.header('Access-Control-Allow-Origin', '*')
                 response = utilities.printXMLLocations(fileid)
                 return response
-        
-                                        
+
+
 class FileHandler(Handler):
-        
+
         def GET(self):
                 """A form for submitting a pdf"""
                 return """<html><head></head><body>
@@ -157,7 +158,7 @@ class FileHandler(Handler):
                 <input type="submit" />
                 </form>
                 </body></html>"""
-                
+
         def POST(self):
                 """Actually submits the file"""
                 try:
@@ -177,14 +178,14 @@ class FileHandler(Handler):
                 except ValueError as ex:
                         web.debug(ex)
                         web.ctx.status = '400'
-                        return "File too large. Limit is ", cgi.maxlen    
+                        return "File too large. Limit is ", cgi.maxlen
 
         def DELETE(self,fileid):
-                
+
                 """ 404 when txt file doesn't exist """
-                if not os.path.exists(TMP_FOLDER + fileid + '.txt'): 
+                if not os.path.exists(TMP_FOLDER + fileid + '.txt'):
                         return web.notfound()
-                
+
                 try:
                         os.unlink(TMP_FOLDER + fileid)
                         os.unlink(TMP_FOLDER + fileid + '.txt')
@@ -199,7 +200,7 @@ class PDFStreamHandler(Handler):
         def POST(self):
                 """Posts a PDF bytestream"""
                 content_size = -1
-                
+
                 # Check for Content-Length header
                 try:
                         content_size = int(web.ctx.env.get('CONTENT_LENGTH'))
@@ -213,7 +214,7 @@ class PDFStreamHandler(Handler):
                 except ValueError as ex:
                         web.debug(ex)
                         web.ctx.status = '400'
-                        return "File too large. Limit is ", cgi.maxlen              
+                        return "File too large. Limit is ", cgi.maxlen
                 try:
                         if content_size == 0: #No Content-Length header
                                 raise ValueError
@@ -221,11 +222,12 @@ class PDFStreamHandler(Handler):
                         web.debug(ex)
                         web.ctx.status = '400'
                         return "Please set Content-Length header for bytestream upload"
-                
+
                 try:
-                        data = web.data()
+                        data = web.input(myfile={})
+
                         with tempfile.NamedTemporaryFile('wb',dir=TMP_FOLDER,delete=False) as f:
-                                f.write(data)
+                                f.write(data.myfile.file.read())
                                 pdfpath = os.path.abspath(f.name)
                         web.debug(pdfpath)
                         passed, message = super(PDFStreamHandler, self).fileCheck(pdfpath)
@@ -239,14 +241,13 @@ class PDFStreamHandler(Handler):
                         web.debug(ex)
                         web.ctx.status = '500'
                         return web.internalerror()
-                
+
 if __name__ == "__main__":
 
         if os.path.isdir(TMP_FOLDER): #Create the temp folder
                 shutil.rmtree(TMP_FOLDER)
-                
+
         os.mkdir(TMP_FOLDER, 0o700)
-                
-        app = web.application(urls, globals()) 
+
+        app = web.application(urls, globals())
         app.run()
-        
